89604c6b3346a49206230b2d7ef57c9625c80257
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2023-11-11 13:24:55 -0700

    prototype functions created testing needed

--- api/src/index.js
+++ api/src/index.js
@@ -8,15 +8,106 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-function punch(request, env, ctx) {
-	env.TSDB.prepare("INSERT INTO punches (user, punch) VALUES (?, ?)").run(request.headers.get("user"), request.headers.get("punch"));
+async function punch(request, env, ctx) {
+	const reqval = await req.json();
+	const userID = reqval.userID;
+	const punchCatagory = reqval.punchCatagory;
+
+	const sql = `
+	BEGIN TRANSACTION;
+
+	WITH latest_punch AS (
+			SELECT punchID
+			FROM timeCardPunches
+			WHERE userID = ? AND punchCatagory = ?
+			ORDER BY punchInTime DESC
+			LIMIT 1
+	)
+	UPDATE timeCardPunches
+	SET punchOutTime = CURRENT_TIMESTAMP
+	WHERE punchID = (SELECT punchID FROM latest_punch) AND punchOutTime IS NULL;
+
+	INSERT INTO timeCardPunches (userID, punchCatagory)
+	SELECT ?, ?
+	WHERE NOT EXISTS (SELECT 1 FROM latest_punch WHERE punchOutTime IS NULL);
+
+	COMMIT;
+	`;
+
+	await env.TSDB.prepare(sql).run(userID, punchCatagory, userID, punchCatagory);
+
+	return new Response("OK");
 }
 
+async function punches(request, env, ctx) {
+	const reqval = await req.json();
+	const userID = reqval.userID;
+	const punchCatagory = reqval.punchCatagory;
+
+	const sql = `
+	SELECT punchInTime, punchOutTime
+	FROM timeCardPunches
+	WHERE userID = ? AND punchCatagory = ?
+	ORDER BY punchInTime DESC
+	LIMIT 10;
+	`;
+
+	const rows = await env.TSDB.prepare(sql).all(userID, punchCatagory);
+
+	return new Response(JSON.stringify(rows), {
+		headers: { "content-type": "application/json" },
+	});
+}
+
+async function update(request, env, ctx) {
+	const reqval = await request.json();
+	const userID = reqval.userID;
+	const punchCatagory = reqval.punchCatagory;
+	const punchInTime = reqval.punchInTime;
+	const punchOutTime = reqval.punchOutTime;
+
+	const sql = `
+	UPDATE timeCardPunches
+	SET punchInTime = COALESCE(?, punchInTime),
+			punchOutTime = COALESCE(?, punchOutTime)
+	WHERE userID = ? AND punchCatagory = ?
+	AND (punchInTime IS NOT NULL OR punchOutTime IS NOT NULL);
+	`;
+
+	await env.TSDB.prepare(sql).run(punchInTime, punchOutTime, userID, punchCatagory);
+
+	return new Response("OK");
+}
+
+async function seconds(request, env, ctx) {
+	const reqval = await req.json();
+	const userID = reqval.userID;
+	const punchCatagory = reqval.punchCatagory;
+	const startDate = reqval.startDate;
+	const endDate = reqval.endDate;
+
+	const sql = `
+	SELECT SUM(strftime('%s', punchOutTime) - strftime('%s', punchInTime)) AS totalSeconds
+	FROM timeCardPunches
+	WHERE userID = ? AND punchCatagory = ? AND punchInTime >= ? AND punchOutTime <= ?
+	`;
+
+	const rows = await env.TSDB.prepare(sql).all(userID, punchCatagory, startDate, endDate);
+
+	return new Response(JSON.stringify(rows), {
+		headers: { "content-type": "application/json" },
+	});
+}
+
+
+
 const routes = {
-	"GET /": () => new Response("This is the api, go to the main site at <a href=\"https://lineonline.app\">lineonline.app</a>", {
+	"GET /": () => new Response("This is the api, go to the main site at <a href=\"https://punch.variablef.com\">punch.variablef.com</a>", {
 		headers: { "content-type": "text/html" },
 	}),
 	"POST /punch": punch,
+	"GET /punches": punches,
+	"GET /seconds": seconds,
 }
 
 export default {
